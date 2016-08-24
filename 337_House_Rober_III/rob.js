b8dd57bc4292b0203ac338a45c8eb53fba738309
halzhan <halzhan163@163.com>
2016-08-25 00:07:45 +0800

    some bugs

--- 337_House_Rober_III/rob.js
+++ 337_House_Rober_III/rob.js
@@ -18,20 +18,30 @@ var isValid = function(val) {
     return false;
 };
 // array to tree
-var toTree = function(arr, idx, node) {
-    if (isValid(arr[idx])) {
-        // 左孩子
-        var leftIdx = idx * 2 + 1;
-        if (isValid(arr[leftIdx])) {
-            node.left = new TreeNode(arr[leftIdx]);
-            toTree(arr, leftIdx, node.left);
+var toTree = function(arr) {
+    if (arr.length) {
+        var rootNode = new TreeNode(arr[0]);
+        var stack = [rootNode];
+        var idx = 1;
+        while (stack.length && idx < arr.length) {
+            var curNode = stack.pop();
+            var num1 = arr[idx];
+            idx++;
+            var num2 = arr[idx];
+            idx++;
+            if (isValid(num2)) {
+                curNode.right = new TreeNode(num2);
+                stack.push(curNode.right);
+            }
+            if (isValid(num1)) {
+                curNode.left = new TreeNode(num1);
+                stack.push(curNode.left);
+            }
+            console.log('curNode: ', curNode);
         }
-        // 右孩子
-        var rightIdx = idx * 2 + 2;
-        if (isValid(arr[rightIdx])) {
-            node.right = new TreeNode(arr[rightIdx]);
-            toTree(arr, rightIdx, node.right);
-        }
+        return rootNode;
+    } else {
+        return null;
     }
 };
 /**
@@ -39,54 +49,23 @@ var toTree = function(arr, idx, node) {
  * @return {number}
  */
 var rob = function(root) {
-    // 跳着遍历
-    var doSearch = function(node, stack) {
-        if (node ) {
-            stack.push(node.val);
+    // 遍历
+    var doSearch = function(node, list) {
+        if (node) {
+            list.push(node.val);
             var left = node.left;
             var right = node.right;
             if (left && left.left) {
-	            var left2 = left.left;
-            	var left3 = left2.left;
-            	if(left3) {
-            		var left4 = left3.left;
-            		var left4Val = left4 ? left4.val : 0;
-            		var tmp2 = node.val + left.val + left4Val;
-            		var tmp3 = node.val + left3.val;
-            		console.log('tmp2: ', tmp2, ' tmp3: ', tmp3);
-            		if(node.val + left2.val + left4Val >= node.val + left3.val) {
-            			doSearch(left2, stack);
-            		}
-            		else {
-            			doSearch(left3, stack);
-            		}
-            	}
-            	else {
-	            	doSearch(left2, stack);
-            	}
+                doSearch(left.left, list);
             }
             if (left && left.right) {
-                doSearch(left.right, stack);
+                doSearch(left.right, list);
             }
             if (right && right.left) {
-                doSearch(right.left, stack);
+                doSearch(right.left, list);
             }
             if (right && right.right) {
-            	var right2 = right.right;
-            	var right3 = right2.right;
-            	if(right3) {
-            		var right4 = right3.right;
-            		var right4Val = right4 ? right4.val : 0;
-            		if(node.val + right2.val + right4Val >= node.val + right3.val) {
-            			doSearch(right2, stack);
-            		}
-            		else {
-            			doSearch(right3, stack);
-            		}
-            	}
-            	else {
-	                doSearch(right2, stack);
-            	}
+                doSearch(right.right, list);
             }
         }
     };
@@ -98,19 +77,71 @@ var rob = function(root) {
         }
         return ans;
     };
-    // 从根节点出发
-    var listRoot = [];
-    doSearch(root, listRoot);
-    // 从左右子节点出发
-    var listChildren = [];
-    if(root) {
-	    doSearch(root.left, listChildren);
-	    doSearch(root.right, listChildren);
+    // 求和比较
+    var maxArrsSum = function(rootVal, arrs1, arrs2) {
+        var ans = 0;
+        rootVal = rootVal || 0;
+        for (var j = 0; j < arrs2.length; j++) {
+            var tmp = doSum(arrs2[j]);
+            for (var i = 0; i < arrs1.length; i++) {
+                var tmp2 = doSum(arrs1[i]);
+                var tmpSum = tmp + tmp2;
+                if (i && j) {
+                    tmpSum += rootVal;
+                }
+                ans = ans >= tmpSum ? ans : tmpSum;
+            }
+        }
+        return ans;
+    };
+    // 从左右孩子出发
+    var rootVal = root ? root.val : 0;
+    var leftLists = [
+        [],
+        [],
+        []
+    ];
+    var rightLists = [
+        [],
+        [],
+        []
+    ];
+    if (root) {
+        // 左孩子开始
+        doSearch(root.left, leftLists[0]);
+        // 左孙子
+        if (root.left) {
+            var left2 = root.left.left;
+            var leftr = root.left.right;
+            if (left2) {
+                doSearch(left2, leftLists[1]);
+            }
+            if (leftr) {
+                doSearch(leftr, leftLists[2]);
+            }
+        }
+        // 右孩子开始
+        doSearch(root.right, rightLists[0]);
+        // 右孙子
+        if (root.right) {
+            var right2 = root.right.right;
+            var rightl = root.right.left;
+            if (rightl) {
+                doSearch(rightl, rightLists[1]);
+            }
+            if (right2) {
+                doSearch(right2, rightLists[2]);
+            }
+        }
     }
-    return Math.max(doSum(listRoot), doSum(listChildren));
+    return maxArrsSum(rootVal, leftLists, rightLists);
 };
 
-var arr = [4,1,null,2,null,3];
-var rootNode = new TreeNode(arr[0]);
-root = toTree(arr, 0, rootNode);
+// [4,1,null,2,null,3]
+// [4,null,3,null,1,2]
+var arr = [2, 1, 3, null, 4];
+// var arr = [4,null,3,null,1,2]; 
+console.log('array: ', arr);
+var rootNode = toTree(arr);
+console.log(rootNode);
 console.log(rob(rootNode));
