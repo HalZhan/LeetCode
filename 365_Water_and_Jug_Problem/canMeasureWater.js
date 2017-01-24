8d6ffa9790ed1ae387216c2c826217344301e7c4
张浩 <halzhan@zhanghaodeMacBook-Pro.local>
2017-01-24 10:26:08 +0800

    365

--- 365_Water_and_Jug_Problem/canMeasureWater.js
+++ 365_Water_and_Jug_Problem/canMeasureWater.js
@@ -4,18 +4,28 @@
  * @param {number} z
  * @return {boolean}
  */
-var canMeasureWater = function(x, y, z) {
-	if(z === 0) {
-		return true;
-	}
-	if(x+y < z || (x === 0 && y === 0)) {
+var canMeasureWater = function (x, y, z) {
+	const findDivisor = function (a, b) {
+		while (b) {
+			let c = a % b;
+			a = b;
+			b = c;
+		}
+		return a;
+	};
+	const divisor = findDivisor(x, y);
+	if (z > x + y) {
 		return false;
 	}
-	// var big = Math.max(x, y);
-	// var small = Math.min(x, y);
-	// var gap = big - small;
-	// if(z === x || z === y || z === x+y || z === gap || (z+big)%small === 0 || z%gap === 0) {
-	// 	return true;
-	// }
-	return true;
+	else if (z === x || z === y || z === 0) {
+		return true;
+	}
+	return (z % divisor === 0);
 };
+
+// let x = 3, y = 5, z = 4; // true
+// let x = 2, y = 6, z = 5; // false;
+// let x = 1, y = 1, z = 12; // false
+let x = 1, y = 2, z = 3; // true
+// let x = 4, y = 6, z =8; // true
+console.log(canMeasureWater(x, y, z));
